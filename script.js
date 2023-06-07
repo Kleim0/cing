21d53a61781ddea668ee50a67fa08d35922b2e96
Kleim0 <100975793+Kleim0@users.noreply.github.com>
2023-06-07 07:19:48 +0000

    the big change

--- script.js
+++ script.js
@@ -5,19 +5,52 @@ canvas.height = window.innerHeight;
 const cw = canvas.width;
 const ch = canvas.height;
 
+let deathWav = new Audio('sounds/death.wav');
+let beginWav = new Audio('sounds/begin.wav');
+let restartWav = new Audio('sounds/restart.wav');
+
 let game = {
 	paused: false,
 	spawnrate: 1000,
 	score: 0,
 	deaths: 0,
 
+	start: function start() {
+		document.getElementById('thing').remove();
+		canvas.removeAttribute('onclick');
+		let l = setInterval('spawn()', 1000);
+
+		addEventListener('mousemove', function(event) {
+			mouse.x = event.x;
+			mouse.y = event.y;
+			player.rotation = Math.atan2(mouse.y - player.y, mouse.x - player.x);
+		});
+
+		addEventListener('mousedown', function() {
+			if (!game.paused) {
+				player.shoot();	
+			}
+		})
+		
+		beginWav.play();
+		requestAnimationFrame(update);
+  },
+	
 	over: function() {
+		popup();
 		this.deaths += 1;
-		this.score = 0;
 		this.paused = true;
-		player.health = player.maxHealth;
 		bullets = [];
 		enemies = [];
+		deathWav.play();
+	},
+
+	restart: function() {
+		this.paused = false;
+		this.score = 0;
+		player.health = player.maxHealth;
+		popupContainer.remove();
+		restartWav.play();
 	},
 
 	drawText: function() {
@@ -25,7 +58,7 @@ let game = {
 		c.font = '50px consolas';
 		c.fillText(this.score, 10, 50);
 		c.fillText(this.deaths, cw - 40, 50);
-	}
+	},
 }
 
 let mouse = {
@@ -34,8 +67,8 @@ let mouse = {
 }
 
 let player = {
-	x: cw/2,
-	y: ch/2,
+	x: cw / 2,
+	y: ch / 2,
 	radius: 20,
 	barrelWidth: 10,
 	barrelLength: 10,
@@ -51,7 +84,7 @@ let player = {
 		c.rotate(this.rotation);
 		c.strokeStyle = this.color;
 		c.beginPath();
-		c.rect(20, -this.barrelWidth/2, this.barrelLength, this.barrelWidth);
+		c.rect(20, -this.barrelWidth / 2, this.barrelLength, this.barrelWidth);
 		c.stroke();
 		c.closePath();
 		c.beginPath();
@@ -155,14 +188,14 @@ function Enemy(x, y, dx, dy) {
 	}
 }
 
-function displayHealth(x, y, maxHealth, curHealth, radius) {
+function displayHealth(x, y, maxHealth, curHealth, radius) {  //displays health below things
 	c.beginPath();
-	c.rect(x-15, y+radius+15, 30, 5);
+	c.rect(x - 15, y + radius + 15, 30, 5);
 	c.fillStyle = 'red';
 	c.fill();
 	c.closePath();
 	c.beginPath();
-	c.rect(x-15, y+radius+15, curHealth/maxHealth * 30, 5);
+	c.rect(x - 15, y + radius + 15, curHealth / maxHealth * 30, 5);
 	c.fillStyle = 'lightgreen';
 	c.fill();
 	c.closePath();
@@ -171,7 +204,7 @@ function displayHealth(x, y, maxHealth, curHealth, radius) {
 function getCollision(one, two) {
 	let a = one.x - two.x;
 	let b = one.y - two.y;
-	let distance = Math.sqrt(a*a + b*b);
+	let distance = Math.sqrt(a * a + b * b);
 	if (distance < one.radius + two.radius) {
 		return true;
 	}
@@ -181,44 +214,51 @@ function getCollision(one, two) {
 }
 
 function spawn() {
-	let k = 0;
-	let x = Math.random() * cw;
-	let y = Math.random() * ch;
-	let angle = Math.atan2(player.y - y, player.x - x);
-	let dx = Math.cos(angle);
-	let dy = Math.sin(angle);
+	if (game.paused == false && document.hasFocus()) {
+		let x = Math.random() * (cw + 400) - 200;  //shit spawning (fix later)
+		let y = Math.random() * (ch + 400) - 200;
+		if (x > 0 && x < cw && y > 0 && y < ch) { // <----
+			spawn();
+			return;
+		};
+		let angle = Math.atan2(player.y - y, player.x - x);
+		let dx = Math.cos(angle);
+		let dy = Math.sin(angle);
 
-	enemies.push(new Enemy(x, y, dx, dy));
+		enemies.push(new Enemy(x, y, dx, dy));
+	}
 }
 
-function start() {
-	let l = setInterval('spawn()', 1000);
-	requestAnimationFrame(update);
-};
+function popup() {
+	let popupContainer = document.createElement('div');
+	document.body.appendChild(popupContainer);
+	popupContainer.id = 'popupContainer';
+	let popup = document.createElement('div');
+	popupContainer.appendChild(popup);
+	popup.id = 'popup';
+	let popupRestart = document.createElement('div');
+	popupContainer.appendChild(popupRestart);
+	popupRestart.id = 'popupRestart';
+	popupRestart.innerHTML = 'Restart';
+	popupRestart.setAttribute('onclick', 'game.restart()');
+	let popupText = document.createElement('div');
+	popup.appendChild(popupText);
+	popupText.id = 'popupText';
+	popupText.innerHTML = 'You Died! <br> Score: ' + game.score;
+	
+}
 
 function update() {
-	if (game.paused == false) {
-	c.clearRect(0, 0, cw, ch);
-	player.update();
-	game.drawText();
-	for (i of bullets) {
-		i.update();
-	}
-	for (i of enemies) {
-		i.update();
-	}
+	if (game.paused == false && document.hasFocus()) {
+		c.clearRect(0, 0, cw, ch);
+		player.update();
+		game.drawText();
+		for (i of bullets) {
+			i.update();
+		}
+		for (i of enemies) {
+			i.update();
+		}
 	}
 	requestAnimationFrame(update);
 };
-
-
-addEventListener('mousemove', function(event) {
-	mouse.x = event.x;
-	mouse.y = event.y;
-	player.rotation = Math.atan2(mouse.y - player.y, mouse.x - player.x);
-});
-
-addEventListener('mousedown', function() {
-	player.shoot();
-})
-start();
